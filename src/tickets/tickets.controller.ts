c77eef0d5e3a38cabeb0189ee097c4147df42558
Hadyan Qashidi <hadyan.qashidi@gmail.com>
2025-06-26 10:01:02 +0800

    Add strikeOff type

--- src/tickets/tickets.controller.ts
+++ src/tickets/tickets.controller.ts
@@ -1,3 +1,4 @@
+import { Op } from 'sequelize';
 import { Body, ConflictException, Controller, Get, Post } from '@nestjs/common';
 import { Company } from '../../db/models/Company';
 import {
@@ -33,32 +34,92 @@ export class TicketsController {
   async create(@Body() newTicketDto: newTicketDto) {
     const { type, companyId } = newTicketDto;
 
-    const category =
-      type === TicketType.managementReport
-        ? TicketCategory.accounting
-        : TicketCategory.corporate;
-
-    const userRole =
-      type === TicketType.managementReport
-        ? UserRole.accountant
-        : UserRole.corporateSecretary;
-
     const assignees = await User.findAll({
-      where: { companyId, role: userRole },
+      where: { companyId },
       order: [['createdAt', 'DESC']],
     });
+    let assignee: User;
+    const existingAccountants: User[] = [];
+    const existingCorporateSecretaries: User[] = [];
+    const existingDirectors: User[] = [];
+    assignees.forEach((a) => {
+      switch (a.role) {
+        case UserRole.accountant:
+          existingAccountants.push(a);
+          break;
+        case UserRole.corporateSecretary:
+          existingCorporateSecretaries.push(a);
+          break;
+        case UserRole.director:
+          existingDirectors.push(a);
+          break;
+      }
+    });
 
-    if (!assignees.length)
+    let category: TicketCategory;
+    let userRole: UserRole;
+    switch (type) {
+      case TicketType.strikeOff:
+        if (existingDirectors.length > 1)
+          throw new ConflictException(
+            `Multiple users with role ${UserRole.director}. Cannot create a ticket`,
+          );
+
+        userRole = UserRole.director;
+        assignee = existingDirectors[0];
+        category = TicketCategory.management;
+        break;
+      case TicketType.managementReport:
+        category = TicketCategory.accounting;
+        userRole = UserRole.accountant;
+
+        if (!existingAccountants.length)
+          throw new ConflictException(
+            `Cannot find user with role ${UserRole.accountant} to create a ticket`,
+          );
+        assignee = existingAccountants[0];
+        break;
+      case TicketType.registrationAddressChange: {
+        const checkExisting = await Ticket.count({
+          where: {
+            companyId,
+            type,
+          },
+        });
+        if (checkExisting > 0)
+          throw new ConflictException(
+            `Registration address change already exists`,
+          );
+
+        userRole = UserRole.corporateSecretary;
+        if (existingCorporateSecretaries.length) {
+          assignee = existingCorporateSecretaries[0];
+        } else {
+          if (existingDirectors.length > 1)
+            throw new ConflictException(
+              `Multiple users with role ${UserRole.director}. Cannot create a ticket`,
+            );
+
+          assignee = existingDirectors[0];
+        }
+
+        category = TicketCategory.corporate;
+        break;
+      }
+    }
+
+    if (
+      userRole === UserRole.corporateSecretary &&
+      existingCorporateSecretaries.length > 1
+    )
+      throw new ConflictException(
+        `Multiple users with role ${UserRole.corporateSecretary}. Cannot create a ticket`,
+      );
+
+    if (!assignee)
       throw new ConflictException(
         `Cannot find user with role ${userRole} to create a ticket`,
       );
-
-    if (userRole === UserRole.corporateSecretary && assignees.length > 1)
-      throw new ConflictException(
-        `Multiple users with role ${userRole}. Cannot create a ticket`,
-      );
-
-    const assignee = assignees[0];
 
     const ticket = await Ticket.create({
       companyId,
@@ -67,6 +128,21 @@ export class TicketsController {
       type,
       status: TicketStatus.open,
     });
+
+    if (type === TicketType.strikeOff) {
+      void Ticket.update(
+        { status: TicketStatus.resolved },
+        {
+          where: {
+            companyId,
+            status: TicketStatus.open,
+            id: {
+              [Op.ne]: ticket.id,
+            },
+          },
+        },
+      );
+    }
 
     const ticketDto: TicketDto = {
       id: ticket.id,
